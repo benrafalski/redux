0e089b7c946d893a90750444b18ec013a87e9012
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-08-09 19:00:33 -0700

    todo state added

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import '../styles/Input.css'
+import { useDispatch } from 'react-redux'
+import { saveTodo } from '../features/todoSlice'
 
 const Input = () => {
     const [input, setInput] = useState('')
+    const dispatch = useDispatch()
     
     const addTodo = () => {
-
+        console.log(`adding ${input}`)
+        dispatch(saveTodo({
+            item: input,
+            done: false,
+            id: Date.now(),
+        }))
     }
 
     return (
---
+++ src/features/todoSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// const [todoList, setTodoList] = useState([])
+// const [state, state.todoList.push(action.payload)] = {
+//     todoList: []
+// }
+
+
+// empty to start, like saying "useState([])"
+const initialState = {
+    todoList: []
+}
+
+const todoSlice = createSlice({
+    name: 'todos',
+    initialState,
+    // function for the todos
+    reducers: {
+        // state = current state of the todo, action = what is being executed
+        saveTodo: (state, action) => {
+            // like saying setTodoList
+            state.todoList.push(action.payload)
+        }
+    }
+});
+
+export const { saveTodo } = todoSlice.actions
+export default todoSlice.reducer
