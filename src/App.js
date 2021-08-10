9f272d045d18ce87107a840c343b551a4809a461
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-08-09 18:47:36 -0700

    input box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Input from './components/Input';
 
 function App() {
   return (
-    <div className="App">
-      <h1>redux tutorial</h1>
+    <div className="app">
+      {/* todo list */}
+      {/* done comp */}
+      {/* input box */}
+      <Input/>
     </div>
   );
 }
